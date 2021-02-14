2ec858c01e87ac5419f9ca4c0fa23b9c6a9843f8
christianoen <ChristianOen@pm.me>
2021-02-14 13:49:48 -0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'christianoen@icloud.com';
+  $receiving_email_address = 'christianoen@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
+  /*
   $contact->smtp = array(
     'host' => 'smtp.mail.me.com',
     'username' => 'christianoen@icloud.com',
     'password' => 'rwkx-azoj-ofqo-kukd',
     'port' => '587'
   );
-  
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
